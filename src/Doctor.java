3e3036463734923b7cc03c17102692377520c809
gaby gramajo <ggabrieldeveloper@gmail.com>
2022-09-29 10:03:15 -0300

    encapsulation and nested classes

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     static int id = 0; // Autoincrement
-    String name;
-    String speciality;
+    private String name;
+    private String email;
+    private String speciality;
 
     Doctor() {
 
@@ -11,10 +15,62 @@ public class Doctor {
         this.name = name;
         this.speciality = speciality;
     }
+
     public void showName() {
         System.out.println(name);
     }
     public void showId() {
         System.out.println("ID Doctor: " + id);
+    }
+
+    // arraylist para almacenar la citas
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    // método para agendar citas
+    public void  addAvailableAppointment(Date date, String time){
+        availableAppointments.add(new AvailableAppointment(date, time));
+        // Agregamos con .add() la cita en el arrayList
+    }
+    // método para devolver el array de citas
+    public ArrayList<AvailableAppointment> getAvailableAppointments(){
+        return availableAppointments;
+    }
+
+    // Clases Estatica anidada para citas disponibles
+    public static class AvailableAppointment {
+        private  int id;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public String showAppointment() {
+            return "Date: " + date + " - Time: " + time;
+        }
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
 
         Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatría");
+        //agendando citas
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
+        //mostrar citas disponibles
+//        for (Doctor.AvailableAppointment appointment : myDoctor.getAvailableAppointments() ) {
+//            System.out.println(appointment.showAppointment());
+//        }
 
-        showMenu();
-
+        Patient patient = new Patient("Alejandra", "alejandra@email.com");
+        patient.setWeight(54.6);
+//        System.out.println(patient.getWeight());;
+        patient.setHeight(1.65);
+//        System.out.println(patient.getHeight());;
+        patient.setPhoneNumber("12344327");
+//        System.out.println(patient.getPhoneNumber());
+        for ( DayOfTheWeek day : DayOfTheWeek.values() ){
+            System.out.println(day.getDayInSpanish());
+        }
     }
 }
