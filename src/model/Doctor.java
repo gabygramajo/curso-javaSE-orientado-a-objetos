489b177e4aecbe1e4d7d23199bb31a846f786242
gaby gramajo <ggabrieldeveloper@gmail.com>
2022-09-30 12:50:22 -0300

    add class Nurse

---
+++ src/model/Doctor.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Doctor extends User {
+    private String speciality;
+
+    Doctor(String name, String email, String speciality) {
+        super(name, email);
+        this.speciality = speciality;
+    }
+
+
+    // arraylist para almacenar la citas
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    // método para agendar citas
+    public void  addAvailableAppointment(Date date, String time){
+        availableAppointments.add(new AvailableAppointment(date, time));
+        // Agregamos con .add() la cita en el arrayList
+    }
+    // método para devolver el array de citas
+    public ArrayList<AvailableAppointment> getAvailableAppointments(){
+        return availableAppointments;
+    }
+
+    // Clases Estatica anidada para citas disponibles
+    public static class AvailableAppointment {
+        private  int id;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public String showAppointment() {
+            return "Date: " + date + " - Time: " + time;
+        }
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+        @Override
+        public String toString() {
+            return "\nDate = " + date +
+                    ", Time = '" + time;
+        }
+    }
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                super.toString() +
+                "speciality='" + speciality +
+                ", available Appointments" + availableAppointments +
+                '}';
+    }
+}
