e8d55c8ede2ef0bd2e5313f93aaccf0ca478d9b4
gaby gramajo <ggabrieldeveloper@gmail.com>
2022-10-03 20:52:21 -0300

    add collections list adn set

--- src/otherConcepts/CollectionArrayList.java
+++ src/otherConcepts/CollectionArrayList.java
@@ -1,24 +1,44 @@
 package otherConcepts;
 
 import model.Doctor;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+//La clase ArrayList es una matriz de tamaño variable , que se puede encontrar en el java.utilpaquete.
 
-import java.util.List;
-
-//import java.util.Iterator;
-
-public class ArrayList {
+public class CollectionArrayList {
     public static void main(String[] args) {
-        Doctor doctor1 = new Doctor("Anahí Salgado", "anahidoc@email.com","Pediatría");
-        Doctor doctor2 = new Doctor("Sergio Donado", "donado@email.com","Cirugía Pediátrica");
+        Doctor doctor1 = new Doctor("Sergio Donado", "donado@email.com","Cirugía Pediátrica");
+        Doctor doctor2 = new Doctor("Anahí Salgado", "anahidoc@email.com","Pediatría");
         Doctor doctor3 = new Doctor("Mariana Rojas", "mrojas@email.com","Neumología");
         Doctor doctor4 = new Doctor("Luiz Dominguez", "dominguezluiz@email.com","Dermatología");
 
-        List<Doctor> listDoctors = new java.util.ArrayList<Doctor>();
+        ArrayList<Doctor> listDoctors = new java.util.ArrayList<>();
+
+        // add objects
         listDoctors.add(doctor1);
         listDoctors.add(doctor2);
         listDoctors.add(doctor3);
         listDoctors.add(doctor4);
 
+        System.out.println( listDoctors.size() );
+        //out-> 4
+
+        //Access an Item
+        System.out.println( listDoctors.get(2).getName() );
+        //out-> Mariana Rojas
+
+        //Change an Item
+        Doctor doctor5 = new Doctor("Lucas Montoya", "lmontoya@email.com", "Neumología");
+        listDoctors.set(2, doctor5);
+        System.out.println( listDoctors.get(2).getName() );
+        //-> Lucas Montoya
+
+        //Remove an Item
+        listDoctors.remove(2);
+        System.out.println( listDoctors.size() );
+        //-> 3
+        // remove all: listDoctors.clear();
 
         // Recorrer
         System.out.println("------- forEach -------");
@@ -28,6 +48,16 @@ public class ArrayList {
                     " - Especialidad: " + doctor.getSpeciality() );
         }
 
+        // sort
+        Collections.sort(listDoctors, new Comparator<Doctor>() {
+            @Override
+            public int compare(Doctor doc1, Doctor doc2) {
+
+                return doc1.getName().compareTo(doc2.getName());
+            }
+        });
+
+        System.out.println("Ordenado: ");
         System.out.println("------- fori -------");
 
         for (int i = 0; i < listDoctors.size(); i++) {
@@ -36,10 +66,3 @@ public class ArrayList {
         }
     }
 }
-
-//    Iterator <Integer> it = map.keySet().iterator();
-//
-//    while( it.hasNext() ){
-//        Integer key = it.next();
-//        System.out.println("Clave: " + key + " -> Valor: " + map.get(key));
-//    }
--- src/otherConcepts/CollectionSet.java
+++ src/otherConcepts/CollectionSet.java
@@ -1,7 +1,47 @@
 package otherConcepts;
 
-public class ListSet {
+import java.util.HashSet;
+// Un HashSet es una colección de elementos donde cada elemento es único y se encuentra en el paquete java.util
+
+public class CollectionSet {
     public static void main(String[] args) {
-        ListSet<String> setNombres = new ListSet<String>();
+
+        HashSet<String> cars = new HashSet<>();
+
+        // Agregar objetos con add()
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("BMW"); // duplicado
+        cars.add("Mazda");
+        System.out.println( cars );
+        //out-> [Volvo, Mazda, Ford, BMW]
+
+        // chequear su existe un valor con .contains()
+        System.out.println( cars.contains("Mazda") );
+        //out-> true
+        System.out.println( cars.contains("Fiat") );
+        //out-> false
+
+        // remover un valor con .remove()
+        cars.remove("Volvo");
+        System.out.println( cars );
+        //out-> [Mazda, Ford, BMW]
+        // remove all: cars.clear();
+
+
+        // Tamaño:
+        System.out.println( cars.size() );
+        //out-> 3
+
+        // Recorrer un HashSet con ForEach
+        for (String car : cars ) {
+            System.out.println( car );
+        }
+        /*out->
+            Mazda
+            Ford
+            BMW
+         */
     }
 }
